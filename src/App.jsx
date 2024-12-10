abe624347b1e3f32321c586890a02479d1d2a07f
Beatrice Ricardos <beatricer@missionreadyhq.com>
2024-12-10 14:58:55 +0800

    Added an H2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
     <h1>This will be deployed.</h1>
+    <h2 style={{backgroundColor: "skyblue", color: "orchid"}}>This has been added after the Netlify Deploy</h2>
     </>
   )
 }
